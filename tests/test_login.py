e4600ea7a69bfad6ffb50b6e9b165288d1b31b01
HiteshKongadi <hiteshkn96@gmail.com>
2023-03-02 12:29:37 +0530

    reusability optimization

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from assertpy import assert_that
@@ -5,22 +6,24 @@ from selenium.webdriver.common.by import By
 
 
 class TestLoginUI:
-    def test_title(self):
+
+    @pytest.fixture(scope="function" , autouse=True)
+    def setup(self):
         # service = Service(executable_path==r"<filepath_of_chrome>")
         # driver = webdriver.Chrome(Service=service)
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(30)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        actual_title = driver.title
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(30)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
+    def test_title(self):
+        actual_title = self.driver.title
         assert "HRM" in actual_title == "OrangeHRM"
         assert_that("OrangeHRM").is_equal_to(actual_title)
 
     def test_header(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(30)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
         # get the login header and assert it
-        actual_header = driver.find_element(By.XPATH, "//h5[contains(@class,'login')]").text
+        actual_header = self.driver.find_element(By.XPATH, "//h5[contains(@class,'login')]").text
         assert_that("Login").is_equal_to(actual_header)
