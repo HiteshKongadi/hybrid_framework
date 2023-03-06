5940dd8b15f79192ff1d7dfe8046af74f8633808
HiteshKongadi <hiteshkn96@gmail.com>
2023-03-06 11:13:28 +0530

    validating login and dashboard verification

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,46 @@
+import time
+
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 
+# @pytest.fixture(scope="class", autouse=True)
+# def scope_class():
+# print("class triggered")
+# yield
+# print("class end")
+
+
+class TestLogin:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        # service = Service(executable_path==r"<filepath_of_chrome>")
+        # driver = webdriver.Chrome(Service=service)
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(30)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+        actual_dashboard = self.driver.find_element(By.XPATH,"//h6").text
+        assert_that("Dashboard").is_equal_to(actual_dashboard)
+        time.sleep(10)
+
+        #print("valid login")
+
+
 class TestLoginUI:
 
-    @pytest.fixture(scope="function" , autouse=True)
+    @pytest.fixture(scope="function", autouse=True)
     def setup(self):
         # service = Service(executable_path==r"<filepath_of_chrome>")
         # driver = webdriver.Chrome(Service=service)
