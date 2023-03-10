57b02687d983db8cc760d8b8497836f7f1e98063
HiteshKongadi <hiteshkn96@gmail.com>
2023-03-10 10:42:05 +0530

    Working with excel

--- utilities/read_utils.py
+++ utilities/read_utils.py
@@ -1,6 +1,12 @@
+"""All read write activities on files can be done using this module"""
 import pandas as pd
 
 
 def get_csv_as_list(file_path):
     df = pd.read_csv(filepath_or_buffer=file_path, delimiter=";")
+    return df.values.tolist()
+
+
+def get_excel_as_list(file_path, sheetname):
+    df = pd.read_excel(io=file_path, sheet_name=sheetname)
     return df.values.tolist()
