47a1f82a1080ca80db287aa1391683395833e520
HiteshKongadi <hiteshkn96@gmail.com>
2023-03-10 11:07:28 +0530

    Updating data into excel and reading it in the TC.

--- utilities/data_source.py
+++ utilities/data_source.py
@@ -2,11 +2,8 @@ from utilities import read_utils
 
 test_invalid_login_data = read_utils.get_csv_as_list("../test_data/Test_invalid_login_data.csv")
 
+test_employee_data = read_utils.get_excel_as_list("../test_data/orange_test_data.xlsx", "test_add_valid_employee")
 # test_employee_data = [
 # ("Admin", "admin123", "Peter", "j", "Wick", "Peter Wick", "Peter"),
 # ("Admin", "admin123", "John", "J", "Wick", "John Wick", "John")
 # ]
-
-
-test_employee_data = read_utils.get_excel_as_list("../test_data/orange_test_data.xlsx", "test_add_valid_employee")
-print(test_employee_data)
